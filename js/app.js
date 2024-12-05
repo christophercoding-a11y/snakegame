8cbe105b462ec6b91bf6cc7d4695e3dbdc86a5a0
christophercodinga11y <christopherjack14@gmail.com>
2024-12-05 10:40:40 -0600

    first pass

--- js/app.js
+++ js/app.js
@@ -25,16 +25,25 @@ class Game {
         this.speed = 100
 
         this.changingDirection= false
+
+        this.foodX = 0
+        this.foodY = 0
     }
 
     init() {
         
+        if (this.hasGameEnded()) {
+            return
+        }
+
+
+        this.changingDirection = false
         // make a timer
         /** setTimeout(callback function, time in ms) */
         setTimeout(()=> {
             this.makeCanvas()
             this.drawSnake()
-            
+            this.drawFood()
             this.moveSnake()
             
 
@@ -68,6 +77,21 @@ class Game {
         })
     }
 
+    // generate food
+    generateFood() {
+        this.foodX = this.randomFood(0, this.snakeBoard.width - 10)
+        this.foodY = this.randomFood(0, this.snakeBoard.height - 10)
+
+        this.snake.forEach(part => {
+            // collision
+            const hasEaten = part.x === this.foodX && part.y === this.foodY
+
+            if (hasEaten) {
+                this.generateFood()
+            }
+        })
+    }
+
     // 3 move snake
     moveSnake() {
         const snake = this.snake
@@ -78,10 +102,34 @@ class Game {
 
         snake.unshift(head)
 
-        snake.pop()
+        // when snake eats food
+        const hasEatenFood = snake[0].x === this.foodX && snake[0].y === this.foodY
+
+        if (hasEatenFood) {
+            this.generateFood()
+        } else {
+            snake.pop()
+        }
+
     }
 
-    // change direction
+    hasGameEnded() {
+        const snake = this.snake
+        const snakeBoard = this.snakeBoard
+
+        for (let i = 4; i < snake.length; i++) {
+            if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true
+        }
+
+        const hitLeftWall = snake[0].x < 0
+        const hitRightWall = snake[0].x > snakeBoard.width - 10
+        const hitTopWall = snake[0].y < 0
+        const hitBottomWall = snake[0].y > snakeBoard.height - 10
+
+        return hitLeftWall || hitRightWall || hitTopWall || hitBottomWall
+    }
+
+    // 4 change direction
     changeDirection(e) {
         
 
@@ -94,7 +142,6 @@ class Game {
         this.changingDirection = true
 
         const keyPressed = e.keyCode
-        console.log(keyPressed)
 
         const goingUp = this.dy === -10
         const goingDown = this.dy === 10
@@ -107,8 +154,8 @@ class Game {
         }
 
         if (keyPressed === UP && !goingDown) {
-            this.dx = -10
-            this.dy = 0
+            this.dx = 0
+            this.dy = -10
         }
 
         if (keyPressed === RIGHT && !goingLeft) {
@@ -121,7 +168,20 @@ class Game {
             this.dy = 10
         }
     }
+
+    // draw food
+    drawFood() {
+        this.snakeBoardCtx.fillStyle = 'darkgoldenrod'
+        this.snakeBoardCtx.strokeStyle = 'limegreen'
+        this.snakeBoardCtx.fillRect(this.foodX, this.foodY, 10 , 10)
+        this.snakeBoardCtx.strokeRect(this.foodX, this.foodY, 10, 10)
+    }
+
+    randomFood(min, max) {
+        return Math.round((Math.random()*(max - min) + min) / 10) * 10
+    }
 }
+
 
 const snake = new Game()
 
